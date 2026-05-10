ebe6c0107b1510ce7126106f132a86c6a040a63b
keyshaunb7 <personkeyshaun@gmail.com>
2026-05-10 01:22:58 +0100

    Started calculator mini project

---
+++ Chapter02_Types_Operators_Expressions/Mini Projects/calculator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define IN 1
+#define OUT 0
+int calculator(char[]);
+int main ()
+{
+
+    char sampleString [] ="20 + 20";
+    printf("%s = %d",sampleString , calculator(sampleString));
+
+
+
+    return 0;
+}
+
+
+int calculator(char equationString [] )
+{
+    int i ,num, result;
+    int numState= IN;
+    i ,result = 0;
+
+    while (equationString[i] != '\0' ){
+        printf("Result at start of loop: %d\n", result);
+        if (isdigit(equationString[i]) && numState == IN)
+        {
+            numState = IN;
+            num = 10 * num + (equationString[i]- '0'); 
+            printf("%d\n",equationString[i]- '0') ;
+        }
+
+        else if (equationString[i] == '+')
+        {
+            numState = OUT;
+            num = 0;
+            
+        }
+        i++;
+        
+        result+= num;
+        printf("Num  == %d \n" , num); 
+        printf("Result = %d\n",result);
+        
+    }
+    
+    return result;
+}
